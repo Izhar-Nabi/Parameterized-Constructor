8cb069d4aa24051ff7afbee9887b4e7e44624d2d
Izhar-Zour <90437474+Izhar-Nabi@users.noreply.github.com>
2021-11-20 17:02:44 +0500

    Parameterized Constructor

---
+++ MyHobby.java
@@ -0,0 +1,13 @@
+class MyHobby
+{	
+	public static void main(String args[])	{
+	Player me = new Player("Cricket");
+	System.out.println("My Hobby is "+me.Hob);}
+}
+	class Player{
+	Player(String hub){
+	Hob=hub;
+	System.out.println("Coding is My Hobby");
+	}
+	String Hob; 
+		}
